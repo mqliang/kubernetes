288be995b253f3155212109cf5729e36c1f99cb3
Deyuan Deng <deyuan.deng@gmail.com>
2015-05-31 19:44:27 -0400

    Add anchang cloudprovider

---
+++ pkg/cloudprovider/anchang/doc.go
@@ -0,0 +1,7 @@
+/*
+Copyright 2015 Caicloud All rights reserved.
+*/
+
+// Package anchang_cloud is an implementation of Interface, TCPLoadBalancer
+// and Instances for Anchange Cloud Platform.
+package anchang_cloud
