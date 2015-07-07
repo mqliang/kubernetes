09516fd7b33c6611a3576eda04170e2219cac68f
Pengcheng Tang <pctang@lab126.com>
2015-07-06 21:58:56 -0700

    Change the order of resource creation. Also change resource creation to foreground.

--- cluster/anchnet/addons/addons-start.sh
+++ cluster/anchnet/addons/addons-start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-function start_addon() {
+function create_resource_from_file() {
   tries=$2
   delay=$3
   while [ ${tries} -gt 0 ]; do
     sudo /opt/bin/kubectl create -f $1 && \
+      echo "== Successfully started $1 at $(date -Is)" && \
       return 0;
     let tries=tries-1;
     echo "== Failed to start $1. ${tries} tries remaining. =="
@@ -15,7 +16,12 @@ function start_addon() {
 
 mkdir -p ~/kube/addons
 mv ~/kube/system:dns-secret ~/kube/skydns-rc.yaml ~/kube/skydns-svc.yaml ~/kube/addons
-for obj in $(find ~/kube/addons -type f);do
-  start_addon ${obj} 10 10 &
+
+# Create secret before the addons which have dependencies on it.
+create_resource_from_file ~/kube/addons/system:dns-secret 10 10
+
+# Create addons from file
+for obj in $(find ~/kube/addons -type f -name \*.yaml -o -name \*.json);do
+  create_resource_from_file ${obj} 10 10
   echo "++ obj ${obj} is created ++"
 done
