16f329971c2b3d01ed5948194f4583ace0bee6fc
keontang <ikeontang@gmail.com>
2017-01-13 21:48:00 +0800

    Update sync-images.sh by s/build/build-tools/ and s/index.caicloud.io/cargo.caicloud.io/

--- hack/caicloud/sync-images.sh
+++ hack/caicloud/sync-images.sh
@@ -46,11 +46,11 @@ function sync_image {
 function replace_special_images {
   # Special handling for gcr.io/google_containers/kube-cross:vX.Y.Z, which
   # uses an environment variable 'KUBE_BUILD_IMAGE_CROSS_TAG' defined in
-  # build/common.sh to decide its tag number.
-  source ${KUBE_ROOT}/build/common.sh
-  cp ${KUBE_ROOT}/build/build-image/Dockerfile ${KUBE_ROOT}/build/build-image/Dockerfile.bk
-  perl -X -i -pe "s/KUBE_BUILD_IMAGE_CROSS_TAG/${KUBE_BUILD_IMAGE_CROSS_TAG}/g" ${KUBE_ROOT}/build/build-image/Dockerfile
-  trap "mv ${KUBE_ROOT}/build/build-image/Dockerfile.bk ${KUBE_ROOT}/build/build-image/Dockerfile" EXIT
+  # build-tools/common.sh to decide its tag number.
+  source ${KUBE_ROOT}/build-tools/common.sh
+  cp ${KUBE_ROOT}/build-tools/build-image/Dockerfile ${KUBE_ROOT}/build-tools/build-image/Dockerfile.bk
+  perl -X -i -pe "s/KUBE_BUILD_IMAGE_CROSS_TAG/${KUBE_BUILD_IMAGE_CROSS_TAG}/g" ${KUBE_ROOT}/build-tools/build-image/Dockerfile
+  trap "mv ${KUBE_ROOT}/build-tools/build-image/Dockerfile.bk ${KUBE_ROOT}/build-tools/build-image/Dockerfile" EXIT
 }
 
 #
@@ -64,7 +64,7 @@ rm -rf $PROCESSED_LOG
 replace_special_images
 
 # Avoid bail out when error pulling/pushing a single image. Must be set after
-# replace_special_images, since it calls source ${KUBE_ROOT}/build/common.sh
+# replace_special_images, since it calls source ${KUBE_ROOT}/build-tools/common.sh
 # which enables errexit.
 set +o errexit
 
@@ -82,10 +82,10 @@ GCR_PREFIXES=(
   "gcr.io/google-samples/"
 )
 CAICLOUD_PREFIXES=(
-  "index.caicloud.io/caicloudgcr/google_containers"
-  "index.caicloud.io/caicloudgcr/google-containers"
-  "index.caicloud.io/caicloudgcr/google_samples"
-  "index.caicloud.io/caicloudgcr/google-samples"
+  "cargo.caicloud.io/caicloudgcr/google_containers"
+  "cargo.caicloud.io/caicloudgcr/google-containers"
+  "cargo.caicloud.io/caicloudgcr/google_samples"
+  "cargo.caicloud.io/caicloudgcr/google-samples"
 )
 for i in `seq "${#PATTERNS[@]}"`; do
   index=$(($i-1))
@@ -105,10 +105,9 @@ for i in `seq "${#PATTERNS[@]}"`; do
        ${KUBE_ROOT}/examples \
        ${KUBE_ROOT}/cluster/addons \
        ${KUBE_ROOT}/cluster/saltbase \
-       ${KUBE_ROOT}/contrib \
        ${KUBE_ROOT}/docs \
        ${KUBE_ROOT}/cmd \
-       ${KUBE_ROOT}/build \
+       ${KUBE_ROOT}/build-tools \
        ${KUBE_ROOT}/test/e2e/testing-manifests | sort -u |
     while read -r gcr_image ; do
       gcr_image=${gcr_image%;}
